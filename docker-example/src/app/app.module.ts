2b2f082737895ae9a29253bbe13a596e803f7da9
antony lulciuc <antony.lulciuc@gmail.com>
2018-01-07 20:42:11 -0500

    added component

--- docker-example/src/app/app.module.ts
+++ docker-example/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
